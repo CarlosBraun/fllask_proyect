3d20c571d4c40d4b3089ac94cd87f3d06edb9635
Carlos Braun <69319659+CarlosBraun@users.noreply.github.com>
2024-04-02 13:52:00 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const connection = require("./database");
 const app = express();
-const PORT = process.env.PORTlet v1 = connection.host;
+const PORT = process.env.PORTlet;
+v1 = connection.host;
 // Ruta de prueba
 app.get("/", (req, res) => {
   res.send("¡Hola, mundo! prueba 1");
 });
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
