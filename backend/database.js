4f6b7d8b9f612028a1c07ce07d62c6afea54cd43
pggodoy <pigodoy1@miuandes.cl>
2024-05-13 11:35:22 -0400

    Temporarily replaced env variables

--- backend/database.js
+++ backend/database.js
@@ -2,11 +2,11 @@ const mysql = require("mysql2/promise");
 
 // Configuracion para obtener una pool de conecciones
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: "flask-g19-miuandes-3b9d.a.aivencloud.com", //process.env.DB_HOST,
+  user: "avnadmin", //process.env.DB_USER,
+  password: "AVNS_LHyyUux2JxRT64CsmA5", //process.env.DB_PASSWORD,
+  database: "defaultdb", //process.env.DB_NAME,
+  port: 18573, //Number(process.env.DB_PORT),
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
